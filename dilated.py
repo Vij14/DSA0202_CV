5d0cbcdbe6f6c76c345557f7f8adf9f3018c745a
Vij14 <113373765+Vij14@users.noreply.github.com>
2023-06-19 17:25:28 +0530

    Add files via upload

--- dilated.py
+++ dilated.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-img = cv2.imread("C:\Users\91999\Downloads.jpg")
+img = cv2.imread("E:/1.jpg")
 edges = cv2.Canny(img, 100, 200)
 kernel = np.ones((5, 5), np.uint8)
 dilated = cv2.dilate(img, kernel, iterations=1)
